7cd01f36ca40c21b4ddfe92a6f33879e7737e875
Stuart Wood <stuartwood2010@hotmail.com>
2021-11-05 10:37:50 -0600

    Generating password, but not giving the right characters

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let generateBtn = document.querySelector("#generate");
 let Upper = true;
 let lower = true;
 let numbers = true;
-let specialchars =true;
+let specialchars = true;
 let characterarray = [];
 
-console.log(characterarray);
-
 function generatePassword() {
+  let randomChars = [];
   /*When the user clicks the generate button, prompt the user to pick a password length between 8 and 128 characters */
   const charlength = prompt("How many characters would you like the password to contain? Minimum = 8, Maximum = 128");
     
@@ -29,9 +28,6 @@ function generatePassword() {
     characterarray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
   }
 
-  /*log the characterarray to the console*/
-  console.log(characterarray);
-
   /*Ask the user if he/she wants to include lowercase letters in the password */
   lower = confirm("Would you like the password to include lowercase letter?");    
 
@@ -39,9 +35,6 @@ function generatePassword() {
   if (lower) {
     characterarray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
   }
-
-  /*log the characterarray to the console*/
-  console.log(characterarray);
 
   /*Ask the user if he/she wants to include numbers in the password */
   numbers = confirm("Would you like the password to include numbers?");
@@ -51,9 +44,6 @@ function generatePassword() {
     characterarray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
   }
 
-  /*log the characterarray to the console*/
-  console.log(characterarray);
-
   /*Ask the user if he/she wants to include special characters in the password */
   specialchars = confirm("Would you like the password to include special characters?");        
 
@@ -61,17 +51,19 @@ function generatePassword() {
   if (specialchars) {
     characterarray.push("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", '"', "}", "~");
   }
-        
-  /*log the characterarray to the console*/
-  console.log(characterarray);
 
   /*Generates a random character from the characterarray and logs it to the console*/
-   for (let i = 0; i < parseInt(charlength); i++){
+     for (let i = 0; i < parseInt(charlength); i++){
      randomchar = Math.floor(Math.random() * characterarray.length);
-     console.log(randomchar, characterarray[randomchar]);
+     /*Adds the random character generated above to the rndomChars array*/
+     randomChars.push(randomchar);
     }
-  
-  
+    /*Logs the randomChars array to the console*/
+    console.log(randomChars);
+    /*Converts the randomChars array into a string and names it password*/
+    let password = randomChars.join("");
+    console.log(password);
+    return password;
 
 }
 // Write password to the #password input
@@ -80,8 +72,8 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
